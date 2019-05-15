3b91f324d4dfb8e65c9318c0c95e1883e7e4bcb2
Joseph Magsajo <magsajo.joseph@gmail.com>
2019-05-15 23:23:52 +0800

    build fix

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ var webpack = require('webpack')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    path: path.resolve(__dirname, './js/dist/'),
+    publicPath: '/js/dist/',
     filename: 'build.js'
   },
   module: {
