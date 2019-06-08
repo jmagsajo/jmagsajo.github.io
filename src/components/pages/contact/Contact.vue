38657e5cc08af0cdbbf3406cbf8fad79dc0cf02b
Joseph Magsajo <magsajo.joseph@gmail.com>
2019-06-08 21:06:59 +0800

    1st phase deployed

--- src/components/pages/contact/Contact.vue
+++ src/components/pages/contact/Contact.vue
@@ -6,6 +6,24 @@
 				<p>Contact me!</p>
 			</div>
 		</div>
+		<div class="container" id="contact-container">
+			<div class="row">
+				<div class="jumbotron">
+					<h2 class="display-4">Contact Details</h2>
+					<p class="lead">Feel free to contact me!</p>
+					<hr class="my-4">
+				  	<p>
+						Email me @ <b>joseph.magsajo03@gmail.com</b>
+					</p>
+					<p class="social-icons">
+						<a href="https://www.facebook.com/hatred.03" target="-blank" class="text-info pb-2 pt-1"><i class="fab fa-facebook-square"></i></a>
+						<a href="https://www.linkedin.com/in/joseph-magsajo-a239a442/" target="-blank" class="text-info pb-2 pt-1"><i class="fab fa-linkedin"></i></a>
+						<a href="https://www.instagram.com/phesoj03/" target="-blank" class="text-info pb-2 pt-1"><i class="fab fa-instagram"></i></a>
+					</p>
+				  	<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+				</div>
+			</div>
+		</div>
 	 </span>
 </template>
 
@@ -29,6 +47,16 @@ export default {
 	    height: 80% !important; 
 	}
 
+	.banner-text{
+		text-align:center;
+	    position: relative;
+    	top: 30%;
+	}
+		.banner-text p{
+		    font-family: 'Pacifico', regular, 'Kaushan Script', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
+		    font-size: 260%;
+		}
+
 	.projects{
 		margin-top: 40px;
 		margin-bottom: 40px;
@@ -38,5 +66,41 @@ export default {
 	    margin-left: 4% !important;
     	margin-right: 4% !important;
     	margin-top: -1.25rem !important;
+	}
+
+	#contact-container{
+		margin-top: 30px;
+	}
+
+	.jumbotron{
+	    width: 1140px;
+	}
+
+	.social-icons{
+		font-size: 30px;
+	}
+
+	@media (max-width: 576px) {
+		.banner-text p{
+		    font-size: 25px;
+		}
+	}
+	
+	@media (min-width: 577px){
+		.banner-text p{
+			font-size: 30px;
+		}
+	}
+
+	@media (max-width: 933px) {
+		.banner-text p{
+			font-size: 30px;
+		}
+	}
+
+	@media (min-width: 934px) {
+		.banner-text p{
+			font-size: 260%;
+		}
 	}
 </style>
--- src/components/pages/portfolio/Portfolio.vue
+++ src/components/pages/portfolio/Portfolio.vue
@@ -13,7 +13,7 @@
 
 					  <!-- Card image -->
 					  <div class="view view-cascade overlay">
-					    <img  class="card-img-top" src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(147).jpg" alt="Card image cap">
+					    <img  class="card-img-top" src="/img/gmovies/gmovies.png" alt="GMovies">
 					    <a>
 					      <div class="mask rgba-white-slight waves-effect waves-light"></div>
 					    </a>
@@ -23,13 +23,13 @@
 					  <div class="card-body card-body-cascade">
 
 					    <!-- Label -->
-					    <h5 class="pink-text pb-2 pt-1"><i class="fas fa-utensils"></i> Culinary</h5>
+					    <h5 class="pink-text pb-2 pt-1"><i class="fas fa-window-restore"></i> Web project</h5>
 					    <!-- Title -->
-					    <h4 class="font-weight-bold card-title">Cheat day inspirations</h4>
+					    <h4 class="font-weight-bold card-title">GMovies</h4>
 					    <!-- Text -->
-					    <p class="card-text">Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi.</p>
+					    <p class="card-text">GMovies is a FREE app for both for Android and iOS devices, GMovies mobile app features services that allow you to stay updated on the current and upcoming movie releases in cinemas nationwide, check daily showtimes, reserve the best seats and book your movie tickets straight on your phones.</p>
 					    <!-- Button -->
-					    <a class="btn btn-unique">Button</a>
+					    <a href="https://www.gmovies.ph/" target="_blank" class="btn btn-unique">Go to site</a>
 
 					  </div>
 
@@ -42,7 +42,7 @@
 
 					  <!-- Card image -->
 					  <div class="view view-cascade overlay">
-					    <img  class="card-img-top" src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(147).jpg" alt="Card image cap">
+					    <img  class="card-img-top" src="/img/sureseats/sureseats.png" alt="Sureseats">
 					    <a>
 					      <div class="mask rgba-white-slight waves-effect waves-light"></div>
 					    </a>
@@ -52,26 +52,27 @@
 					  <div class="card-body card-body-cascade">
 
 					    <!-- Label -->
-					    <h5 class="pink-text pb-2 pt-1"><i class="fas fa-utensils"></i> Culinary</h5>
+					    <h5 class="pink-text pb-2 pt-1"><i class="fas fa-window-restore"></i> Web project</h5>
 					    <!-- Title -->
-					    <h4 class="font-weight-bold card-title">Cheat day inspirations</h4>
+					    <h4 class="font-weight-bold card-title">Sureseats</h4>
 					    <!-- Text -->
-					    <p class="card-text">Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi.</p>
+					    <p class="card-text">Online Reservation for Ayala Malls</p>
 					    <!-- Button -->
-					    <a class="btn btn-unique">Button</a>
+					    <a href="https://www.sureseats.com/" target="_blank" class="btn btn-unique">Go to site</a>
 
 					  </div>
 
 					</div>
 					<!-- Card Narrower -->
 				</div>
+
 				<div class="col-sm projects">
 					<!-- Card Narrower -->
 					<div class="card card-cascade narrower">
 
 					  <!-- Card image -->
 					  <div class="view view-cascade overlay">
-					    <img  class="card-img-top" src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(147).jpg" alt="Card image cap">
+					    <img  class="card-img-top" src="/img/sample/sample1.jpg" alt="Sample 1">
 					    <a>
 					      <div class="mask rgba-white-slight waves-effect waves-light"></div>
 					    </a>
@@ -81,19 +82,24 @@
 					  <div class="card-body card-body-cascade">
 
 					    <!-- Label -->
-					    <h5 class="pink-text pb-2 pt-1"><i class="fas fa-utensils"></i> Culinary</h5>
+					    <h5 class="pink-text pb-2 pt-1"><i class="fas fa-utensils"></i> Open Source</h5>
 					    <!-- Title -->
-					    <h4 class="font-weight-bold card-title">Cheat day inspirations</h4>
+					    <h4 class="font-weight-bold card-title">Hackers Theme based html template</h4>
 					    <!-- Text -->
-					    <p class="card-text">Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi.</p>
+					    <p class="card-text">
+					    	An Open source html template.
+							<br>
+							You can download it <a href="https://github.com/jmagsajo/Hackers-Theme-Sample" target="_blank">here!</a>
+					    </p>
 					    <!-- Button -->
-					    <a class="btn btn-unique">Button</a>
+					    <a href="http://josephmagsajo.atwebpages.com/sample/sample1/index.html" target="_blank" class="btn btn-unique">Go to site</a>
 
 					  </div>
 
 					</div>
 					<!-- Card Narrower -->
 				</div>
+
 			</div>
 		</div>
 	</span>
@@ -119,6 +125,16 @@ export default {
 	    height: 80% !important; 
 	}
 
+	.banner-text{
+		text-align:center;
+	    position: relative;
+    	top: 30%;
+	}
+		.banner-text p{
+		    font-family: 'Pacifico', regular, 'Kaushan Script', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
+		    font-size: 260%;
+		}
+
 	.projects{
 		margin-top: 40px;
 		margin-bottom: 40px;
@@ -128,5 +144,29 @@ export default {
 	    margin-left: 4% !important;
     	margin-right: 4% !important;
     	margin-top: -1.25rem !important;
+	}
+
+	@media (max-width: 576px) {
+		.banner-text p{
+		    font-size: 25px;
+		}
+	}
+	
+	@media (min-width: 577px){
+		.banner-text p{
+			font-size: 30px;
+		}
+	}
+
+	@media (max-width: 933px) {
+		.banner-text p{
+			font-size: 30px;
+		}
+	}
+
+	@media (min-width: 934px) {
+		.banner-text p{
+			font-size: 260%;
+		}
 	}	
 </style>
