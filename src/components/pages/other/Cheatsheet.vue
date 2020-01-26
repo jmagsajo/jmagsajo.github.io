2031e48038b7c7d4f6cd9ab64e07e17da2817f9a
Joseph Magsajo <magsajo.joseph@gmail.com>
2020-01-26 21:31:44 +0800

    updagte

--- src/components/pages/other/Cheatsheet.vue
+++ src/components/pages/other/Cheatsheet.vue
@@ -18,6 +18,21 @@
 						<li><a target="_blank" href="https://www.diycode.cc/projects/FriendsOfPHP/PHP-CS-Fixer">PHP-CS-Fixer</a></li>
 						<li><a href="https://scotch.io/bar-talk/s-o-l-i-d-the-first-five-principles-of-object-oriented-design" target="_blank">S.O.L.I.D</a></li>
 						<li><a href="https://stackoverflow.com/questions/32615861/get-week-number-in-month-from-date-in-php" target="_blank">Get week number in month from date in PHP?</a></li>
+						<li>
+							<a href="https://andy-carter.com/blog/difference-between-include-and-require-statements-in-php" target="_blank">Difference Between 'include' and 'require' Statements in PHP</a>
+						</li>
+						<li>
+							<a href="https://www.cloudways.com/blog/php-security/" target="_blank">Ultimate PHP Security Best Practices</a>
+						</li>
+						<li>
+							<a href="https://stackoverflow.com/questions/1876516/is-it-possible-to-have-an-interface-that-has-private-protected-methods" target="_blank">Is it possible to have an interface that has private / protected methods?</a>
+						</li>
+						<li>
+							<a href="https://phppot.com/php/php-access-modifiers/" target="_blank">PHP Access Modifiers</a>
+						</li>
+						<li>
+							<a href="https://www.startutorial.com/articles/view/5-new-features-in-php-7-that-you-should-have-a-look-at" target="_blank">5 New Features In PHP 7 That You Should Have A Look At</a>
+						</li>
 					</ul>
 				</div>
 
@@ -38,6 +53,9 @@
 						<li><a href="https://dev.mysql.com/doc/refman/8.0/en/views.html" target="_blank">Using Views</a></li>
 						<li><a href="https://dev.mysql.com/doc/refman/8.0/en/view-syntax.html" target="_blank">View Syntax</a></li>
 						<li><a href="http://zetcode.com/databases/mysqltutorial/storageengines/" target="_blank">MySQL storage engines</a></li>
+						<li>
+							<a href="https://www.guru99.com/joins.html" target="_blank">MySQL JOINS Tutorial: INNER, OUTER, LEFT, RIGHT, CROSS</a>
+						</li>
 					</ul>
 				</div>
 
@@ -47,6 +65,30 @@
 						<li><a href="https://laravel.com/docs/6.x" target="_blank">Docs 6.x</a></li>
 						<li><a href="https://lumen.laravel.com/" target="_blank">Lumen</a></li>
 						<li><a href="https://backpackforlaravel.com/" target="_blank">backpack</a></li>
+					</ul>
+				</div>
+
+				<div>
+					<h1>OOP</h1>
+					<ul>
+						<li>
+							<a href="https://www.guru99.com/interface-vs-abstract-class-java.html" target="_blank">Interface vs Abstract Class in Java</a>
+						</li>
+						<li>
+							<a href="https://www.quora.com/What-is-different-between-abstract-class-and-interface-in-PHP" target="_blank">What is different between abstract class and interface in PHP?</a>
+						</li>
+						<li>
+							<a href="https://www.greycampus.com/codelabs/php/OOPS" target="_blank">OBJECT-ORIENTED PROGRAMMING IN PHP</a>
+						</li>
+						<li>
+							<a href="https://medium.com/@hamzzza.ahmed95/four-pillars-of-object-oriented-programming-oop-e8d7822aa219" target="_blank">Four Pillars of Object Oriented Programming</a>
+						</li>
+						<li>
+							<a href="https://stackoverflow.com/questions/3384204/what-are-namespaces" target="_blank">What are namespaces?</a>
+						</li>
+						<li>
+							<a href="https://www.tutorialspoint.com/java/java_polymorphism.htm" target="_blank">Java - Polymorphism</a>
+						</li>
 					</ul>
 				</div>
 
